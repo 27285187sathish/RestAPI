98125222b74389fb932c9821283fbccce778c13b
sathish <sathish@192.168.0.14>
2017-03-25 12:05:31 -0400

    First commit

---
+++ src/test/java/MavenFramework/AppTest.java
@@ -0,0 +1,17 @@
+package MavenFramework;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+
+public class AppTest {
+	@Test
+	public void Vinayaga(){
+		System.out.println("Hi Vinayaga");
+		
+	}
+	
+    
+}
